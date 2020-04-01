fb2aa77788189b93c6a5be85d06fe93dfb4b4263
bj.enderin <bj.enderin@gmail.com>
2020-04-01 17:23:35 +0800

    3

--- uLCD/main.cpp
+++ uLCD/main.cpp
@@ -7,8 +7,9 @@
 #include "uLCD_4DGL.h"
 
 
-uLCD_4DGL uLCD(D1, D0, D2); // serial tx, serial rx, reset pin;
-
+uLCD_4DGL uLCD(D1, D7, D2); // serial tx, serial rx, reset pin;
+PwmOut PWM1(D0);
+Serial pc( USBTX, USBRX );
 
 int main()
 
@@ -16,8 +17,39 @@ int main()
 
     // basic printf demo = 16 by 18 characters on screen
 
-    uLCD.printf("\n107161127\n"); //Default Green on black text
+    uLCD.printf("\n107061127\n"); //Default Green on black text
     uLCD.filled_rectangle(50, 50, 100, 100, 0x00FFFF);
+    
 
+    PWM1.period(0.001);
+    PWM1=0;
+
+    while (1)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            PWM1 = PWM1+0.1;
+            pc.printf("%1.3f\r\n", PWM1);
+            wait(0.1);
+        }
+        
+        for (int i = 0; i < 10; i++)
+        {
+            PWM1 = PWM1-0.1;
+            pc.printf("%1.3f\r\n", PWM1);
+            wait(0.1);
+        }
+        
+    }
+    
+    
 
 }
+
+
+
+
+
+
+ 
+
